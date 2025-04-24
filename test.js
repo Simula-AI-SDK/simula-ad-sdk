21fec6c774ddc67f2f3fdce1dc67ccd498c24831
Athreya Daniel <aadcodes@gmail.com>
2025-04-24 19:24:51 -0400

    sse for minimal options test

--- test.js
+++ test.js
@@ -59,12 +59,17 @@ async function runTests() {
       apiBaseUrl: 'http://127.0.0.1:8000'
     });
 
-    const minimalResult = await minimalAdInjector.insertAd({
-      history: sampleHistory,
-      assistantResponse: sampleResponse
-    });
     console.log('\nMinimal Configuration Response:');
-    console.log(JSON.stringify(minimalResult, null, 2));
+    for await (
+      const chunk of adInjector.insertAd(
+        { 
+          history: sampleHistory,
+          assistantResponse: sampleResponse,
+        }
+      )
+    ){
+      console.log(chunk);
+    }
 
   } catch (error) {
     console.error('Test Error:', error);
